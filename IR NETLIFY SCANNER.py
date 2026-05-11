1e6a21ca580be46e699193789e87bc4c22d0a6e1
IR NETLIFY <mehrzadsivizi@gmail.com>
2026-05-11 09:05:38 +0330

    Update IR NETLIFY SCANNER.py

--- IR NETLIFY SCANNER.py
+++ IR NETLIFY SCANNER.py
@@ -9,48 +9,41 @@ BRAND_NAME = "IR_NETLIFY"
 TELEGRAM_CH = "https://t.me/IR_NETLIFY"
 
 domains = [
-    "helm.sh", "keda.sh", "rook.io", "istio.io", "cilium.io", "fluxcd.io",
-    "harbor.io", "calico.org", "linkerd.io", "openebs.io", "tekton.dev",
-    "longhorn.io", "blog.helm.sh", "docs.helm.sh", "crossplane.io",
-    "kubernetes.io", "kubebuilder.io", "cert-manager.io", "letsencrypt.org",
-    "kind.sigs.k8s.io", "kops.sigs.k8s.io", "krew.sigs.k8s.io",
-    "kwok.sigs.k8s.io", "kueue.sigs.k8s.io", "jobset.sigs.k8s.io",
-    "kaniko.sigs.k8s.io", "minikube.sigs.k8s.io", "operatorframework.io",
-    "container.sigs.k8s.io", "kustomize.sigs.k8s.io", "argo-cd.readthedocs.io",
-    "cluster-api.sigs.k8s.io", "descheduler.sigs.k8s.io", "gateway-api.sigs.k8s.io",
-    "external-dns.sigs.k8s.io", "service-apis.sigs.k8s.io", "image-builder.sigs.k8s.io",
-    "kubectl.docs.kubernetes.io", "metrics-server.sigs.k8s.io",
-    "scheduler-plugins.sigs.k8s.io", "controller-runtime.sigs.k8s.io",
-    "prometheus-operator.sigs.k8s.io", "node-feature-discovery.sigs.k8s.io",
-    "hierarchical-namespaces.sigs.k8s.io", "secrets-store-csi-driver.sigs.k8s.io",
-    "security-profiles-operator.sigs.k8s.io", "cluster-proportional-autoscaler.sigs.k8s.io"
+"cluster-proportional-autoscaler.sigs.k8s.io", "kueue.sigs.k8s.io", "longhorn.io",
+"hierarchical-namespaces.sigs.k8s.io", "operatorframework.io", "blog.helm.sh", "helm.sh",
+"node-feature-discovery.sigs.k8s.io", "minikube.sigs.k8s.io", "kubernetes.io", "keda.sh",
+"image-builder.sigs.k8s.io", "cluster-api.sigs.k8s.io", "kind.sigs.k8s.io", "calico.org",
+"external-dns.sigs.k8s.io", "service-apis.sigs.k8s.io", "kops.sigs.k8s.io", "linkerd.io",
+"security-profiles-operator.sigs.k8s.io", "kaniko.sigs.k8s.io", "docs.helm.sh", "rook.io",
+"secrets-store-csi-driver.sigs.k8s.io", "jobset.sigs.k8s.io", "crossplane.io", "istio.io",
+"prometheus-operator.sigs.k8s.io", "kustomize.sigs.k8s.io", "kubebuilder.io", "cilium.io",
+"controller-runtime.sigs.k8s.io", "container.sigs.k8s.io", "cert-manager.io", "fluxcd.io",
+"scheduler-plugins.sigs.k8s.io", "argo-cd.readthedocs.io", "letsencrypt.org", "harbor.io",
+"kubectl.docs.kubernetes.io", "descheduler.sigs.k8s.io", "krew.sigs.k8s.io", "openebs.io",
+"metrics-server.sigs.k8s.io", "gateway-api.sigs.k8s.io", "kwok.sigs.k8s.io", "tekton.dev"
 ]
 
 ips = list(set([
-    "50.7.5.83", "50.7.87.2", "50.7.87.3", "50.7.87.4", "50.7.87.5",
-    "75.2.60.5", "5.9.210.65", "5.9.248.38", "5.9.248.39", "50.7.85.43",
-    "144.76.1.88", "104.21.33.34", "188.114.98.0", "188.114.99.0",
-    "3.162.247.34", "3.162.247.38", "3.162.247.45", "3.162.247.77",
-    "3.33.186.135", "63.176.8.218", "74.91.29.207", "85.10.207.48",
-    "85.10.207.51", "88.99.249.74", "94.130.33.41", "95.216.69.37",
-    "104.198.14.52", "104.21.63.202", "148.251.65.39", "15.197.167.90",
-    "170.205.28.40", "172.67.150.14", "188.40.147.23", "188.40.181.55",
-    "204.12.196.34", "204.12.196.39", "34.194.97.138", "35.157.26.135",
-    "40.160.22.170", "52.222.214.38", "52.222.214.99", "54.232.119.62",
-    "65.109.34.234", "69.197.138.87", "83.136.211.95", "85.158.145.74",
-    "91.99.175.105", "94.130.70.160", "138.201.54.122", "142.54.178.211",
-    "142.54.178.215", "142.54.189.111", "172.67.158.128", "178.63.240.111",
-    "184.171.110.10", "185.134.23.172", "188.40.254.151", "204.12.192.223",
-    "204.12.223.183", "52.222.214.108", "52.222.214.124", "63.141.252.203",
-    "63.141.252.207", "69.197.146.178", "69.197.146.183", "136.243.128.223",
-    "168.119.202.236", "173.208.128.143", "50.7.5.85", "76.76.21.112",
-    "94.130.13.19", "94.130.50.12", "198.252.206.1", "104.18.25.196",
-    "149.154.167.99", "178.22.122.101", "204.79.197.220", "216.239.38.120"
+"142.54.178.215", "213.180.193.56", "170.205.28.40", "76.76.21.112", "188.114.96.3", "50.7.5.83",
+"142.54.189.111", "216.239.38.120", "172.67.150.14", "85.10.207.48", "188.114.96.6", "50.7.5.85",
+"149.154.167.99", "15.197.167.90", "52.222.214.38", "188.40.147.23", "188.114.97.3", "50.7.87.2",
+"136.243.128.223", "34.194.97.138", "52.222.214.99", "188.40.181.55", "188.114.97.6", "50.7.87.3",
+"168.119.202.236", "35.157.26.135", "54.232.119.62", "198.202.211.1", "188.114.98.0", "50.7.87.4",
+"173.208.128.143", "40.160.22.170", "65.109.34.234", "198.252.206.1", "188.114.99.0", "50.7.87.5",
+"40.114.177.246", "172.67.158.128", "69.197.138.87", "204.12.196.34", "216.198.79.3", "75.2.60.5",
+"64.239.109.193", "185.134.23.172", "104.18.25.196", "3.162.247.38", "94.130.13.19", "5.9.210.65",
+"69.197.146.178", "188.40.254.151", "104.21.60.220", "3.162.247.45", "94.130.33.41", "5.9.248.38",
+"69.197.146.183", "204.12.192.223", "104.21.63.202", "3.162.247.77", "94.130.50.12", "5.9.248.39",
+"138.201.54.122", "204.12.223.183", "104.198.14.52", "63.176.8.218", "95.216.69.37", "50.7.85.43",
+"142.54.178.211", "204.79.197.220", "148.251.65.39", "74.91.29.207", "104.21.33.34", "64.29.17.3",
+"52.222.214.108", "172.67.201.240", "83.136.211.95", "204.12.196.39", "10.10.34.36", "52.250.41.2",
+"52.222.214.124", "178.22.122.101", "85.158.145.74", "216.150.1.193", "37.16.18.81", "76.76.21.21",
+"63.141.252.203", "178.63.240.111", "91.99.175.105", "3.33.186.135", "85.10.207.51", "144.76.1.88",
+"63.141.252.207", "184.171.110.10", "94.130.70.160", "3.162.247.34", "88.99.249.74", "216.24.57.1"
 ]))
 
-MAX_THREADS = 80
+MAX_THREADS = 100
 
-# --- UI STYLING ---
 if os.name == 'nt': os.system('color')
 
 C_CYAN = "\033[96m"
@@ -72,7 +65,6 @@ def show_footer():
     print(f"    Follow us: {TELEGRAM_CH}")
     print(f"{C_CYAN}{'='*60}{RESET}")
 
-# --- CORE LOGIC ---
 def check_pair(ip, domain):
     cmd = ["curl", "-I", "-s", "--max-time", "3", f"https://{domain}", "--resolve", f"{domain}:443:{ip}"]
     try:
@@ -99,8 +91,7 @@ def main():
                 for future in as_completed(futures):
                     res = future.result()
                     completed += 1
-                    
-                    # Modern Progress Line
+
                     sys.stdout.write(f"\r{BOLD}{C_MAGENTA}  [PROGRESS]{RESET} {completed}/{total} | {BOLD}{C_GREEN}FOUND: {found_count}{RESET}  ")
                     sys.stdout.flush()
 
